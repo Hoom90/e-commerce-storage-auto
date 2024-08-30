7396d737b84309818aae87024276fb3aac6d15ae
Hoom90 <mohammad.mahdavi0015@gmail.com>
2024-08-30 18:52:43 +0330

    svg component added

---
+++ src/assets/component/bag_plus.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['width', 'color'])
+</script>
+<template>
+    <svg :width="props.width" :height="props.width" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M12.0009 21.3201H8.43094C7.33237 21.2923 6.27951 20.8746 5.4606 20.1418C4.64169 19.409 4.11011 18.4088 3.96094 17.3201L2.96094 9.32007"
+            :stroke="props.color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M20.9992 9.32007L20.6992 11.8201" :stroke="props.color" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        <path d="M3 9.32004C8.81444 7.20973 15.1856 7.20973 21 9.32004" :stroke="props.color" stroke-width="1.5"
+            stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M6.42969 8.34006L9.07969 3.32007" :stroke="props.color" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        <path d="M17.5699 8.34006L14.9199 3.32007" :stroke="props.color" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        <path d="M19 22.8201V14.8201" :stroke="props.color" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+        <path d="M15 18.8201H23" :stroke="props.color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
